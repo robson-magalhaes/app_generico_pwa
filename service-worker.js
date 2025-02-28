e3aef4617ed556773c61a8fc1780b3dbfa16fef9
Robson Oliveira Magalhães <128374770+robson-magalhaes@users.noreply.github.com>
2025-02-28 11:00:34 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,14 @@
 const CACHE_NAME = 'site-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/app_generico_pwa/',
+  '/app_generico_pwa/index.html',
+  '/app_generico_pwa/styles.css',
+  '/app_generico_pwa/script.js',
+  '/app_generico_pwa/manifest.json',
+  '/app_generico_pwa/icons/icon-192x192.png',
+  '/app_generico_pwa/icons/icon-512x512.png'
 ];
+
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -15,6 +16,20 @@ self.addEventListener('install', (event) => {
       .then((cache) => {
         return cache.addAll(urlsToCache);
       })
+  );
+});
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
